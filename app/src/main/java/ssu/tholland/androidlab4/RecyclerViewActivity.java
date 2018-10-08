f41e3f6d1fdbd6f080caff7443a2a9fe51ae7f62
Jonathan Gil <jonathangil@Jonathans-MacBook-Air.local>
2018-10-07 18:11:41 -0700

    step 17

--- app/src/main/java/ssu/tholland/androidlab4/RecyclerViewActivity.java
+++ app/src/main/java/ssu/tholland/androidlab4/RecyclerViewActivity.java
@@ -33,10 +33,13 @@ public class RecyclerViewActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 RecipeSearchAsyncTask task = new RecipeSearchAsyncTask();
                 task.setListener(new RecipeSearchAsyncTask.RecipeCallbackListener() {
                     @Override
                     public void onRecipeCallback(List<RecipeModel> models) {
+                        RecipeViewAdapter adapter = new RecipeViewAdapter(models);
+                        recyclerView.setAdapter(adapter);
 
                     }
                 });
--- app/src/main/java/ssu/tholland/androidlab4/utils/RecipeParser.java
+++ app/src/main/java/ssu/tholland/androidlab4/utils/RecipeParser.java
@@ -1,8 +1,11 @@
 package ssu.tholland.androidlab4.utils;
 
+import com.google.gson.Gson;
+
 import java.util.List;
 
 import ssu.tholland.androidlab4.model.RecipeModel;
+import ssu.tholland.androidlab4.model.RecipeResponse;
 
 public class RecipeParser {
 
@@ -13,7 +16,9 @@ public class RecipeParser {
         // 2. parse the `json` response (into a RecipeResponse)
 
         // 3. return a List of RecipeModel instead of `null`
-
-        return null;
+        Gson gson = new Gson();
+        RecipeResponse response = gson.fromJson(json,RecipeResponse.class);
+        List<RecipeModel> recipes = response.getRecipes();
+        return recipes;
     }
 }
