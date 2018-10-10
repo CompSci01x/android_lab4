c848e62f4d85e010441c2b183b89ec13684d7077
Connor Littleton <Littletonconnor@gmail.com>
2018-10-10 13:49:09 -0700

    Working Code Complete

--- app/src/main/java/ssu/tholland/androidlab4/RecipeViewAdapter.java
+++ app/src/main/java/ssu/tholland/androidlab4/RecipeViewAdapter.java
@@ -1,38 +1,50 @@
 package ssu.tholland.androidlab4;
 
-import android.support.annotation.NonNull;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.List;
 
 import ssu.tholland.androidlab4.model.RecipeModel;
+import ssu.tholland.androidlab4.network.RecipeSearchAsyncTask;
 
-public class RecipeViewAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
+public class RecyclerViewActivity extends AppCompatActivity {
 
-    private List<RecipeModel> recipeCollection;
-
-    public RecipeViewAdapter(List<RecipeModel> collection) {
-        recipeCollection = collection;
-    }
-
-    @NonNull
-    @Override
-    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View inflatedView = LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.recycler_list_item, parent, false);
-        return new RecipeViewHolder(inflatedView);
-    }
+    private EditText searchEditText;
+    private Button searchButton;
+    private RecyclerView recyclerView;
 
     @Override
-    public void onBindViewHolder(@NonNull RecipeViewHolder recipeViewHolder, int position) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+        searchEditText = findViewById(R.id.search_edit_text);
 
-    }
+        recyclerView = findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
+        recyclerView.setLayoutManager(layoutManager);
 
-    @Override
-    public int getItemCount() {
-        return 0;
+        searchButton = findViewById(R.id.recipe_search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecipeSearchAsyncTask task = new RecipeSearchAsyncTask();
+                task.setListener(new RecipeSearchAsyncTask.RecipeCallbackListener() {
+                    @Override
+                    public void onRecipeCallback(List<RecipeModel> models) {
+                        RecipeViewAdapter adapter = new RecipeViewAdapter(models);
+                        recyclerView.setAdapter(adapter);
+
+                    }
+                });
+                task.execute(searchEditText.getText().toString());
+            }
+        });
+
     }
 }
