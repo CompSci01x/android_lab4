a875af7332e75585d398a7c5d2f0ce9c141ec126
Raven Clayborn <ravenclayborn@Quetzalcoatl.local>
2018-12-01 21:56:06 -0800

    bonus - image loading

--- app/src/main/java/ssu/tholland/androidlab4/RecipeViewHolder.java
+++ app/src/main/java/ssu/tholland/androidlab4/RecipeViewHolder.java
@@ -2,7 +2,10 @@ package ssu.tholland.androidlab4;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import ssu.tholland.androidlab4.model.RecipeModel;
 
@@ -10,17 +13,23 @@ public class RecipeViewHolder extends RecyclerView.ViewHolder {
 
     private TextView itemNameTextView;
     private TextView itemRatingTextView;
+    private ImageView itemImageView;
 
     public RecipeViewHolder(View itemView) {
         super(itemView);
 
         itemNameTextView = itemView.findViewById(R.id.recycler_item_name);
         itemRatingTextView = itemView.findViewById(R.id.recycler_item_rating);
+        itemImageView = itemView.findViewById(R.id.recipe_image);
 
     }
 
     public void bindView(RecipeModel model) {
         itemNameTextView.setText(model.getRecipeName());
         itemRatingTextView.setText(String.format("Rating: %d / 5", model.getRating()));
+        Picasso.get()
+                .load(model.getSmallImageUrls().get(0))
+                .resize(50, 50)
+                .into(itemImageView);
     }
 }
